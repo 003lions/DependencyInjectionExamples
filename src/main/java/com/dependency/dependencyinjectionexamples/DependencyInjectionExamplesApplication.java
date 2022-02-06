3748c5402d4cd6c9582666d85617064490d5038c
003lions <003lions@gmail.com>
2022-02-05 23:44:12 -0600

    Initial commit

---
+++ src/main/java/com/dependency/dependencyinjectionexamples/DependencyInjectionExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.dependency.dependencyinjectionexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionExamplesApplication.class, args);
+    }
+
+}
